d79a3d1f2daed0b79ff1a93fb6f5e5f8184000f6
yayo <jiwoowatch@gmail.com>
2026-04-18 18:32:33 +0900

    Add Material Components theme dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
     implementation("androidx.core:core-splashscreen:1.0.1")
