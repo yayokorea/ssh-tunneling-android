c80df800b684515472ee8f4f1b37cdea79f66575
yayo <jiwoowatch@gmail.com>
2026-09-16 07:22:58 +0900

    fix: configure Kotlin compiler target with AGP 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
@@ -9,6 +11,12 @@ val releaseKeyAlias = System.getenv("ANDROID_KEY_ALIAS")
 val releaseKeyPassword = System.getenv("ANDROID_KEY_PASSWORD")
 val ciVersionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 1
 val ciVersionName = System.getenv("VERSION_NAME") ?: "1.0"
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
 
 android {
     namespace = "com.yayo.sshtunneling"
@@ -60,10 +68,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     buildFeatures {
