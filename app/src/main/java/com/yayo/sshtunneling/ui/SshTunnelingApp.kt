9abd090a029126cc7a098d395a72501e6929fb6c
yayo <jiwoowatch@gmail.com>
2026-09-16 14:52:50 +0900

    fix: remove invalid compose weight import

--- app/src/main/java/com/yayo/sshtunneling/ui/SshTunnelingApp.kt
+++ app/src/main/java/com/yayo/sshtunneling/ui/SshTunnelingApp.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
